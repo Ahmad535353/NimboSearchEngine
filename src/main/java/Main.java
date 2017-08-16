f94a74af7ac770d84ffaf25486b3dbcb0dbe363c
Ahmad535353 <mohammadkhani.ahmad53@gmail.com>
2017-08-16 17:05:07 +0430

    Crawler without kafka and.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,78 +1,104 @@
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import org.jsoup.Jsoup;
-import org.jsoup.select.Elements;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    //            **** Cache ****
+    static LoadingCache<String,Boolean> cacheLoader;
+    //            **** Cache ****
+
+    //            **** Q ****
+    static ArrayBlockingQueue<String > queue;
+
+    //            **** Q ****
+
+    //            **** writer ****
+    static PrintWriter writer = null;
+    //            **** writer ****
+
     public static void main(String args[]){
 
-//            **** Cache ****
-        LoadingCache<String,Boolean> cacheLoader = CacheBuilder.newBuilder().expireAfterWrite(3, TimeUnit.MINUTES)
+        cacheLoader = CacheBuilder.newBuilder().expireAfterWrite(30, TimeUnit.SECONDS)
                 .build(new CacheLoader<String, Boolean>() {
                     @Override
                     public Boolean load(String key) throws Exception {
-                        return Boolean.TRUE;
+                        return Boolean.FALSE;
                     }
                 });
-//            **** Cache ****
-
-
-//            **** Q ****
-        ArrayBlockingQueue<String > queue = new ArrayBlockingQueue<String>(10000);
-        queue.add("https://en.wikipedia.org/wiki/Main_Page");
-//            **** Q ****
-
-
-        PrintWriter writer = null;
-        int fileName = 0;
-        org.jsoup.nodes.Document doc = null;
-        Elements elements = null;
-
-
-        while (queue.size() != 0){
-            try {
-                String link = queue.take();
-                URL url = new URL(link);
-                String domain = url.getHost();
-                Boolean var = cacheLoader.getIfPresent(domain);
-                if (var == null){
-                    try {
-                        doc = Jsoup.connect(link).get();
-                        cacheLoader.get(domain);
-                        elements = doc.select("a[href]");
-                        writer = new PrintWriter("the-file-name" + fileName + ".txt", "UTF-8");
-                        fileName++;
-                        for (org.jsoup.nodes.Element element : elements){
-                            writer.println(element.attr("title"));
-                            writer.println(element.attr("abs:href") + "\n\n");
-                            queue.add(element.attr("abs:href"));
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                else {
-                    queue.add(link);
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-
+        queue = new ArrayBlockingQueue<String>(50000);
+        try {
+            writer = new PrintWriter("the-file-name0.txt", "UTF-8");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
+//            **** Q ****
+        queue.add("https://en.wikipedia.org/wiki/Main_Page");
+        queue.add("https://us.yahoo.com/");
+        queue.add("https://www.nytimes.com/");
+        queue.add("https://www.msn.com/en-us/news");
+        queue.add("http://www.telegraph.co.uk/news/");
+//            **** Q ****
+
+        long time = System.currentTimeMillis();
+        ArrayList<ParserThread> threadList = new ArrayList<ParserThread>();
+        for (int i = 0 ; i < 8 ; i++){
+
+            ParserThread parserThread = new ParserThread(cacheLoader, queue, writer, i);
+            threadList.add(parserThread);
+        }
+        for (int i = 0 ; i < 8 ; i++){
+            threadList.get(i).joinThread();
+        }
+        time = System.currentTimeMillis() - time;
+        System.out.println(time);
+
+//        int fileName = 0;
+//        org.jsoup.nodes.Document doc = null;
+//        Elements elements = null;
+//
+//
+//        while (queue.size() != 0){
+//            try {
+//                String link = queue.take();
+//                URL url = new URL(link);
+//                String domain = url.getHost();
+//                Boolean var = cacheLoader.getIfPresent(domain);
+//                if (var == null){
+//                    try {
+//                        doc = Jsoup.connect(link).get();
+//                        cacheLoader.get(domain);
+//                        elements = doc.select("a[href]");
+//                        writer = new PrintWriter("the-file-name" + fileName + ".txt", "UTF-8");
+//                        fileName++;
+//                        for (org.jsoup.nodes.Element element : elements){
+//                            writer.println(element.attr("title"));
+//                            writer.println(element.attr("abs:href") + "\n\n");
+//                            queue.add(element.attr("abs:href"));
+//                        }
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//                else {
+//                    queue.add(link);
+//                }
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            } catch (ExecutionException e) {
+//                e.printStackTrace();
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            }
+//
+//        }
     }
 }
--- src/main/java/ParserThread.java
+++ src/main/java/ParserThread.java
@@ -1,21 +1,82 @@
 import com.google.common.cache.LoadingCache;
+import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutionException;
 
 public class ParserThread implements Runnable{
     private Thread thread;
     private LoadingCache<String,Boolean> cacheLoader;
-    private ArrayBlockingQueue<String > q;
-    public int time;
+    private ArrayBlockingQueue<String > queue;
+    public int[] time ;
+    PrintWriter writer = null;
+    public int threadNum;
     public void run() {
         org.jsoup.nodes.Document doc = null;
         Elements elements = null;
+
+
+        for (int i = 0 ; i < 20;){
+            try {
+                String link = queue.take();
+                URL url = new URL(link);
+                java.net.URLEncoder.encode(String.valueOf(url), "UTF-8");
+                String domain = url.getHost();
+                Boolean var = cacheLoader.getIfPresent(domain);
+                if (var == null){
+                    cacheLoader.get(domain);
+                    try {
+                        doc = Jsoup.connect(link).userAgent("Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0").ignoreHttpErrors(true).get();
+                        i++;
+                        writer.println("Thread" + threadNum + " parsed:");
+                        System.out.println("Thread" + threadNum + " parsed:");
+                        writer.println(link);
+                        System.out.println(link);
+                        System.out.println(domain);
+                        System.out.println(i);
+                        elements = doc.select("a[href]");
+                        for (org.jsoup.nodes.Element element : elements){
+                            queue.add(element.attr("abs:href"));
+                        }
+                    } catch (IOException e) {
+//                        e.printStackTrace();
+                    }
+                }
+                else {
+                    System.out.println("Thread" + threadNum + " didn't connected due to LRUCache to " + link);
+                    queue.add(link);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
-    public ParserThread(LoadingCache cache1, ArrayBlockingQueue q1){
+    public void joinThread(){
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public ParserThread(LoadingCache cache1, ArrayBlockingQueue q1, PrintWriter writer, int threadNum){
         this.cacheLoader = cache1;
-        this.q = q1;
+        this.queue = q1;
+        this.writer = writer;
+        this.threadNum = threadNum;
+        this.time = time;
         thread = new Thread(this);
         thread.start();
     }
