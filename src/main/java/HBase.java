2d15d14885cba96650dba3817cc8a175ceacfb5e
Ahmad535353 <mohammadkhani.ahmad53@gmail.com>
2017-08-19 10:05:33 +0430

    Crawler without kafka and Hbase

--- src/main/java/HBase.java
+++ src/main/java/HBase.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class HBase {
     private HashMap<String , String> urlsStorage = new HashMap<String, String>();
-    public void addParsedData(String url , String text , String title ){
+    public void addParsedData(String url , String title ){
         urlsStorage.put(url, url);
     }
     public boolean check(String url){
