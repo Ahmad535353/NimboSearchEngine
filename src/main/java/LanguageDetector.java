74dcbe5d2affe644fd8688d1b511765600da5076
Hosein Qotbodini <hossein.ghotboddini@gmail.com>
2017-08-23 13:06:16 +0430

    Add new_version.

--- src/main/java/LanguageDetector.java
+++ src/main/java/LanguageDetector.java
@@ -1,16 +1,18 @@
 import java.io.UnsupportedEncodingException;
 
 public class LanguageDetector {
-    public Boolean IsEnglish(String str,double d)
+    public static Boolean IsEnglish(String str, double d)
     {
         try {
+            if (str.length() == 0)
+                return false;
             if((str.getBytes("UTF-8").length-str.length())/str.length()<d)
             {
                 return true;
             }
             else return false;
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }
         return true;
     }
